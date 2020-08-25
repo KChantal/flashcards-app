0aef36a2a090818b9d57e357c05a025af770a9ab
Kim Wysocki <kcwysocka@icloud.com>
2020-08-25 12:03:49 -0400

    create Cards Container component

---
+++ client/components/CardContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardsContainer = (props) => {
+  return (
+    <div className="cardsContainer">
+      <h1>Card will go here</h1>
+    </div>
+  );
+};
+
+export default CardsContainer;
