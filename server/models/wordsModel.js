88c7c3908ca886fa2c2cb9f800eac0027a3c1eba
Kim Wysocki <kcwysocka@icloud.com>
2020-08-29 12:39:59 -0400

    set up MongoDB connection and schema for Chinese words

---
+++ server/models/wordsModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { MONGO_URI } = require('../../private/secrets');
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'flashcards',
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error(err));
+
+const Schema = mongoose.Schema;
+
+const chineseSchema = new Schema({
+  english: String,
+  chinese: String,
+  pinyin: String,
+  category: String,
+});
+
+const Chinese = mongoose.model('chinese', chineseSchema);
+
+module.exports = {
+  Chinese,
+};
