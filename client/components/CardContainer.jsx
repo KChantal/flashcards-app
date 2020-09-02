04b708cb66ee93f09d98a19e62a94c60123ecf0d
Kim Wysocki <kcwysocka@icloud.com>
2020-09-02 17:24:02 -0400

    add buttons below flashcard

--- client/components/CardContainer.jsx
+++ client/components/CardContainer.jsx
@@ -5,6 +5,11 @@ const CardsContainer = (props) => {
   return (
     <div className="cards-container">
       <Flashcard />
+
+      <div className="buttons-div">
+        <button className="card-buttons correct-button">我知道</button>
+        <button className="card-buttons wrong-button">我不知道</button>
+      </div>
     </div>
   );
 };
