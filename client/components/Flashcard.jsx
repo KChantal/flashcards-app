1fdf34f45ccf8b2bc9fccd5fed4166ab24ea7526
Kim Wysocki <kcwysocka@icloud.com>
2020-09-04 11:49:06 -0400

    make card flippable onclick instead of onhover

--- client/components/Flashcard.jsx
+++ client/components/Flashcard.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Flashcard = (props) => {
+  const [flipped, setFlipped] = useState(false);
+
+  const flipCard = (event) => {
+    console.log(flipped);
+    setFlipped(!flipped);
+  };
+
   return (
     <div className="container-3D-space">
-      <div className="flashcard">
+      <div
+        className={!flipped ? 'flashcard' : 'flashcard flipped'}
+        onClick={flipCard}
+      >
         <div className="front-of-card">
           <p className="chinese-character">汉字</p>
         </div>
