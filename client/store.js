d405934979f5c8bfed2d7bc3285234a710e3fc01
Kim Wysocki <kcwysocka@icloud.com>
2020-09-07 12:32:55 -0400

    add redux thunk and add composeEnhancer to allow redux dev tools to work in browser

--- client/store.js
+++ client/store.js
@@ -1,7 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import reducers from './reducers/index';
 
-const store = createStore(reducers, composeWithDevTools());
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancer(applyMiddleware(thunk)));
 
 export default store;
